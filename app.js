4da293773aded7c47d7a01a7b0702c34729473ca
Maruf <alenmaruf.am@gmail.com>
2022-02-04 03:37:53 +0600

    Customizing HTTP Requests

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const request = require('postman-request');
 
-const url = 'http://api.weatherstack.com/current?access_key=dde5a18e7933428cdfbb9fa9338272f7&query=New%20York'
+const url = 'http://api.weatherstack.com/current?access_key=dde5a18e7933428cdfbb9fa9338272f7&query=New%20York&units=f'
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data.current);
+request({ url: url, json : true }, (error, response) => {
+    console.log(response.body.current.weather_descriptions[0] + ' .It is currently ' + response.body.current.temperature +' fahrenheit.' + 'But feels like ' + response.body.current.feelslike + ' fahrenheit');
 })
