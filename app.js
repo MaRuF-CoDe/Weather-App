370be692ac4af8d9a97fd7bce802b01b3b282922
Maruf <alenmaruf.am@gmail.com>
2022-02-04 02:35:40 +0600

    Making HTTP Requests

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-console.log('Starting');
+const request = require('postman-request');
 
-setTimeout(() => {
-    console.log('2 second timer');
-}, 2000);
+const url = 'http://api.weatherstack.com/current?access_key=dde5a18e7933428cdfbb9fa9338272f7&query=New%20York'
 
-setTimeout(()=>{
-console.log('0 second Timer');
-},0);
-
-console.log('Stopping');
+request({ url: url }, (error, response) => {
+    const data = JSON.parse(response.body);
+    console.log(data.current);
+})
