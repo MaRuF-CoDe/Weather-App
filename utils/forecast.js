69f19187180426de86b0f47b1429c6654341f8fd
Maruf <alenmaruf.am@gmail.com>
2022-02-05 18:03:07 +0600

    Destructuring and Property Shorthand Challenge

--- utils/forecast.js
+++ utils/forecast.js
@@ -2,13 +2,13 @@ const request = require('postman-request')
 
 const forecast = (latitude,longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=dde5a18e7933428cdfbb9fa9338272f7&query='+ latitude +','+ longitude 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service',undefined)
-        } else if (response.body.error) {
-            callback('Error Code ' + response.body.error.code + '.' + response.body.error.info,undefined);
+        } else if (body.error) {
+            callback('Error Code ' + body.error.code + '.' + body.error.info,undefined);
         } else {
-            callback(undefined,response.body.current.weather_descriptions[0] + ' .It is currently ' + response.body.current.temperature + ' degree.' + 'But feels like ' + response.body.current.feelslike + ' degree');
+            callback(undefined,body.current.weather_descriptions[0] + ' .It is currently ' + body.current.temperature + ' degree.' + 'But feels like ' + body.current.feelslike + ' degree');
                 
         }
     })
