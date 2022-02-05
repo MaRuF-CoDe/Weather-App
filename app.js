6470f00c854913edeef8bbe989b5b8418c0145f8
Maruf <alenmaruf.am@gmail.com>
2022-02-05 17:01:58 +0600

    Callback Chaining

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const geocode = require('./utils/geocode.js');
 const forecast = require('./utils/forecast.js');
 
+const address = process.argv[2]
+
+if (!address) {
+    console.log('Please provide an address');
+} else {
+    geocode(address, (error, data) => {
+        if (error) {
+            return console.log(error);
+        }
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                console.log(error);
+            }
+            console.log(data.location);
+            console.log(forecastData);
+        })
+    })
+}
 
 
-geocode('Los Angles', (error, data) => {
-    console.log('Error : ',error);
-    console.log('Data :',data);
-})
-
-forecast(32.6733,-114.139515, (error, data) => {
-    console.log('Error : ',error);
-    console.log('Data :',data);
-})
